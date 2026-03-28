0dd10e081ec62c1c46214c299785c68c9bbd1c54
Yusuke Ohi <yohi.consadole12@gmail.com>
2026-03-28 11:20:07 +0900

    fix: refine help target output by suppressing filenames and improving formatting

--- common-mk/help.mk
+++ common-mk/help.mk
@@ -3,4 +3,4 @@ help: ## 利用可能なターゲットの一覧を表示します
 	@echo "Usage: make [target]"
 	@echo ""
 	@echo "Targets:"
-	@grep -E '^[a-zA-Z_-]+:.*?## .*$$' $(MAKEFILE_LIST) | sort | awk 'BEGIN {FS = ":.*?## "}; {printf "  \033[36m%-20s\033[0m %s\n", $$1, $$2}'
+	@grep -Eh '^[a-zA-Z_-]+:.*?## .*$$' $(MAKEFILE_LIST) | sort | awk 'BEGIN {FS = ":.*?## "}; {printf "  \033[36m%-25s\033[0m %s\n", $$1, $$2}'
