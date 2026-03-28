99d717bb8edd246b1f1779ab50ba8c611aa6f430
Yusuke Ohi <yohi.consadole12@gmail.com>
2026-03-28 10:14:08 +0900

    feat: add common resources for makefile and project rules

---
+++ common-mk/help.mk
@@ -0,0 +1,6 @@
+.PHONY: help
+help: ## 利用可能なターゲットの一覧を表示します
+	@echo "Usage: make [target]"
+	@echo ""
+	@echo "Targets:"
+	@grep -E '^[a-zA-Z_-]+:.*?## .*$$' $(MAKEFILE_LIST) | sort | awk 'BEGIN {FS = ":.*?## "}; {printf "  \033[36m%-20s\033[0m %s\n", $$1, $$2}'
